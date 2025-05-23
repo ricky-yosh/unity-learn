9bbcbec5efd724f5b799edb14ba5086686840c09
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2025-05-22 22:17:37 -0700

    Test for spacebar press

--- unity/Prototype 2/Assets/Scripts/PlayerController.cs
+++ unity/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     public float xRange = 10.0f;
 
+    public GameObject projectilePrefab;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -25,5 +27,10 @@ public class PlayerController : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch Projectile from the player
+        }
     }
 }
