4a52662f735ddd80ea746e269d6c06797599e581
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2025-05-24 20:26:25 -0700

    Destroy objects on collision

--- unity/Prototype 2/Assets/Scripts/SpawnManager.cs
+++ unity/Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -6,20 +6,25 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalsPrefabs;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S)) {
-            // Randomly generate animal index and spawn position
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalsPrefabs.Length);
-            Instantiate(animalsPrefabs[animalIndex], spawnPos, animalsPrefabs[animalIndex].transform.rotation);
-        }
+
+    }
+
+    void SpawnRandomAnimal()
+    {
+        // Randomly generate animal index and spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int animalIndex = Random.Range(0, animalsPrefabs.Length);
+        Instantiate(animalsPrefabs[animalIndex], spawnPos, animalsPrefabs[animalIndex].transform.rotation);
     }
 }
