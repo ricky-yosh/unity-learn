7d84f7b29c9864c32d46dbd6552afbca0ddcaee0
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2025-06-13 21:31:57 -0700

    Complete mod the cube

--- unity/ModTheCube/Assets/ModTheCube/Cube.cs
+++ unity/ModTheCube/Assets/ModTheCube/Cube.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     public MeshRenderer Renderer;
-    public Color MaterialColor = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+    public Color MaterialColor = new Color(1f, 1.0f, 0.0f, 0.0f);
     
     void Start()
     {
