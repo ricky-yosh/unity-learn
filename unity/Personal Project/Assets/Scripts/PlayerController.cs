acb01c8e26bfb0c7d3f173d247e31e25ab3d2035
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2025-06-20 08:54:31 -0700

    Constrain the Player’s movement

--- unity/Personal Project/Assets/Scripts/PlayerController.cs
+++ unity/Personal Project/Assets/Scripts/PlayerController.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f;
+    public float speed = 20f;
     private Rigidbody playerRb;
-    public float horizontalInput;
-    public float verticalInput;
+    private float horizontalInput;
+    private float verticalInput;
+    private float xBounds = 23.15f;
+    private float zBoundsTop = 3.84f;
+    private float zBoundsBottom = -18.12f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,9 +19,26 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.x < -xBounds)
+        {
+            transform.position = new Vector3(-xBounds, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xBounds)
+        {
+            transform.position = new Vector3(xBounds, transform.position.y, transform.position.z);
+        }
+        if (transform.position.z > zBoundsTop)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBoundsTop);
+        }
+        if (transform.position.z < zBoundsBottom)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBoundsBottom);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
         Vector3 move = transform.right * horizontalInput + transform.forward * verticalInput;
         transform.Translate(move * speed * Time.deltaTime);
     }
+
 }
