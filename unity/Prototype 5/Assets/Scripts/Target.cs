90e26b9505462d733d6a720f9220c660a27e3d49
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2025-06-21 22:32:58 -0700

    Destroy target with click and sensor

--- unity/Prototype 5/Assets/Scripts/Target.cs
+++ unity/Prototype 5/Assets/Scripts/Target.cs
@@ -33,6 +33,16 @@ public class Target : MonoBehaviour
         return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
     }
 
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
     // Update is called once per frame
     void Update()
     {
