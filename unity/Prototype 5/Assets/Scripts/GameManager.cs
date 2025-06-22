4abb8936f6011f02a9db127931e43848cb9d06a7
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2025-06-22 09:35:47 -0700

    Make the restart button work

--- unity/Prototype 5/Assets/Scripts/GameManager.cs
+++ unity/Prototype 5/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -23,6 +24,11 @@ public class GameManager : MonoBehaviour
     {
         gameOverText.gameObject.SetActive(true);
         isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     IEnumerator SpawnTarget()
