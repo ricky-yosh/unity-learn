6d39f2c2c4815492f5d4cf018506b3d981084945
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2025-06-22 08:47:19 -0700

    Add a Particle explosion

--- unity/Prototype 5/Assets/Scripts/Target.cs
+++ unity/Prototype 5/Assets/Scripts/Target.cs
@@ -11,6 +11,7 @@ public class Target : MonoBehaviour
     private float ySpawnPos = -6;
     private GameManager gameManager;
     public int pointValue;
+    public ParticleSystem explosionParticle;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -39,6 +40,7 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
         gameManager.UpdateScore(pointValue);
     }
 
