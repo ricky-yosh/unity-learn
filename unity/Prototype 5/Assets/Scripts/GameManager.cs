e4c3e48daaeedb9162507d66db5c2ae4aa656644
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2025-06-22 08:17:00 -0700

    Create a new UpdateScore method

--- unity/Prototype 5/Assets/Scripts/GameManager.cs
+++ unity/Prototype 5/Assets/Scripts/GameManager.cs
@@ -13,8 +13,7 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         StartCoroutine(SpawnTarget());
-        score = 0;
-        scoreText.text = "Score: " + score;
+        UpdateScore(0);
     }
 
     IEnumerator SpawnTarget()
@@ -24,7 +23,14 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
+            UpdateScore(5);
         }
+    }
+
+    private void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
 
     // Update is called once per frame
