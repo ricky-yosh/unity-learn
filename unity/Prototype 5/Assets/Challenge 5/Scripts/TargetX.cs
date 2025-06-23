2307cfd40efedb0d9ee780f9e0084dda6d1d4a43
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2025-06-22 19:06:49 -0700

    Destroy target when mouse is down

--- unity/Prototype 5/Assets/Challenge 5/Scripts/TargetX.cs
+++ unity/Prototype 5/Assets/Challenge 5/Scripts/TargetX.cs
@@ -27,7 +27,7 @@ public class TargetX : MonoBehaviour
     }
 
     // When target is clicked, destroy it, update score, and generate explosion
-    private void OnMouseEnter()
+    private void OnMouseDown()
     {
         if (gameManagerX.isGameActive)
         {
@@ -35,7 +35,7 @@ public class TargetX : MonoBehaviour
             gameManagerX.UpdateScore(pointValue);
             Explode();
         }
-               
+
     }
 
     // Generate a random spawn position based on a random index from 0 to 3
