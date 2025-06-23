ccc090cad80d2e24aed5bf37dcbe21d06c850c1d
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2025-06-22 18:18:30 -0700

    Call SetDifficulty on button click

--- unity/Prototype 5/Assets/Scripts/DifficultyButton.cs
+++ unity/Prototype 5/Assets/Scripts/DifficultyButton.cs
@@ -8,6 +8,12 @@ public class DifficultyButton : MonoBehaviour
     void Start()
     {
         button = GetComponent<Button>();
+        button.onClick.AddListener(SetDifficulty);
+    }
+
+    void SetDifficulty()
+    {
+        Debug.Log(gameObject.name + " was clicked");
     }
 
     // Update is called once per frame
