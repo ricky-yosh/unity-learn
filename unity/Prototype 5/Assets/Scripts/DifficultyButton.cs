713b76dadf9591be14ced116ce3bf6659bde3daf
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2025-06-22 18:06:27 -0700

    Add a DifficultyButton script

---
+++ unity/Prototype 5/Assets/Scripts/DifficultyButton.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButton : MonoBehaviour
+{
+    private Button button;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        button = GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
