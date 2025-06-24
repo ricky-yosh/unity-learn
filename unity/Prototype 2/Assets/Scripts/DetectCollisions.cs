07e5d198441b61f0aea3513bbf4bc24969d35ae9
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2025-06-24 11:22:31 -0700

    Implement object pooling for efficiency

--- unity/Prototype 2/Assets/Scripts/DetectCollisions.cs
+++ unity/Prototype 2/Assets/Scripts/DetectCollisions.cs
@@ -1,22 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DetectCollisions : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     void OnTriggerEnter(Collider other)
     {
+        // Instead of destroying the projectile when it collides with an animal
+        //Destroy(other.gameObject); 
+
+        // Just deactivate the food and destroy the animal
+        other.gameObject.SetActive(false);
         Destroy(gameObject);
-        Destroy(other.gameObject);
     }
+
 }
