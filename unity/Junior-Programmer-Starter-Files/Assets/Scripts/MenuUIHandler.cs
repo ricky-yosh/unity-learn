ce005dd22abe6fc9f62f4bf16a3621df31c3f826
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2025-06-25 16:21:24 -0700

    Add methods to save selected color and save to file

--- unity/Junior-Programmer-Starter-Files/Assets/Scripts/MenuUIHandler.cs
+++ unity/Junior-Programmer-Starter-Files/Assets/Scripts/MenuUIHandler.cs
@@ -16,12 +16,13 @@ public class MenuUIHandler : MonoBehaviour
         // add code here to handle when a color is selected
         MainManager.Instance.TeamColor = color;
     }
-    
+
     private void Start()
     {
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
 
     public void StartNew()
@@ -31,10 +32,22 @@ public class MenuUIHandler : MonoBehaviour
 
     public void Exit()
     {
-    #if UNITY_EDITOR
+        MainManager.Instance.SaveColor();
+#if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
-    #else
+#else
         Application.Quit();
-    #endif
+#endif
+    }
+
+    public void SaveColorClicked()
+    {
+        MainManager.Instance.SaveColor();
+    }
+
+    public void LoadColorClicked()
+    {
+        MainManager.Instance.LoadColor();
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
 }
