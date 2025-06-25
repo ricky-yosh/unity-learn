225c303f2d9726d9895f99989591945a09c72a4d
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2025-06-25 12:16:26 -0700

    Create persistent state manager

---
+++ unity/Junior-Programmer-Starter-Files/Assets/Scripts/MainManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+    public static MainManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
